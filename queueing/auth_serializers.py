5d4b2c67153d362a56e84eb19d1b42973aeebf7d
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-02-20 02:06:08 +0800

    feat: create admin serializer

--- queueing/auth_serializers.py
+++ queueing/auth_serializers.py
@@ -51,3 +51,39 @@ class ChangePasswordSerializer(serializers.Serializer):
         if attrs['new_password'] != attrs['confirm_password']:
             raise serializers.ValidationError({"new_password": "New passwords don't match."})
         return attrs
+
+class CreateAdminSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=User.objects.all())])
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password2', 'email', 'first_name', 'last_name']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            is_staff = True,
+            is_superuser=True,
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        # Create staff profile for admin
+        from .models import StaffProfile
+        StaffProfile.objects.create(
+            user=user,
+            role='admin',
+            can_manage_queue=True
+        )
+        
+        return user
