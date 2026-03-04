e180e3a8040933cd1a1a93d4527effe1f7facd46
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-03-05 02:43:25 +0800

    websockets route

--- queueing/routing.py
+++ queueing/routing.py
@@ -2,7 +2,6 @@ from django.urls import re_path, path
 from . import consumers
 
 websocket_urlpatterns = [
-        path('ws/test/', consumers.TestConsumer.as_asgi()),
     re_path(r'^ws/dashboard/$', consumers.DashboardConsumer.as_asgi()),
     re_path(r'^ws/staff/(?P<service_id>\d+)/$', consumers.StaffDashboardConsumer.as_asgi()),
     re_path(r'^ws/ticket/(?P<ticket_id>[0-9a-f-]+)/$', consumers.TicketStatusConsumer.as_asgi()),
