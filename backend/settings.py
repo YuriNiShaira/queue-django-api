154f7b5ddc58354787551a0720bf1f0ee54a63e9
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-03-10 17:25:17 +0800

    fix cors for vercel frontend

--- backend/settings.py
+++ backend/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True  # For development only
+CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
@@ -90,6 +90,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://localhost:8080",
+    "https://queuick-fe.vercel.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -99,6 +100,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://localhost:8080",
+    "https://queuick-fe.vercel.app",
 ]
 
 
