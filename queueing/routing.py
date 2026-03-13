08c5acb0a5f44c6d7659d7bd6f5ebb81607265c7
Yuri Mauricio <sangeandyasha04@gmail.com>
2026-03-13 14:21:13 +0800

    add service status websocket route

--- queueing/routing.py
+++ queueing/routing.py
@@ -5,4 +5,5 @@ websocket_urlpatterns = [
     re_path(r'^ws/dashboard/$', consumers.DashboardConsumer.as_asgi()),
     re_path(r'^ws/staff/(?P<service_id>\d+)/$', consumers.StaffDashboardConsumer.as_asgi()),
     re_path(r'^ws/ticket/(?P<ticket_id>[0-9a-f-]+)/$', consumers.TicketStatusConsumer.as_asgi()),
+    re_path(r'^ws/service/(?P<service_id>\d+)/status/$', consumers.ServiceStatusConsumer.as_asgi()),
 ]
